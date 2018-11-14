2171d459ed39c25c62b4db8aba2c7085ba62024b
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-14 16:59:37 +0500

    Project Created

---
+++ Ch14-P5-SharedCarLibClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch14_P5_SharedCarLibClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
