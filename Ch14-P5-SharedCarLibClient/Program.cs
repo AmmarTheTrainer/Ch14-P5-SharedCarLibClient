9b23bcd1845a45f63dce5b5293b8347cb7aaa5cd
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-14 19:14:56 +0500

    final update from head office

--- Ch14-P5-SharedCarLibClient/Program.cs
+++ Ch14-P5-SharedCarLibClient/Program.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Ch14_P2_CarLibrary;
+
 namespace Ch14_P5_SharedCarLibClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Shared Assembly Client *****");
+            SportsCar c = new SportsCar();
 
+            c.TurboBoost();
+            Console.ReadLine();
         }
     }
 }
